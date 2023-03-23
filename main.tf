a1f7f0e32870db41b265019d0b5b930b467d5149
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-03-23 16:58:41 +0000

    Test initial db kv secret

--- main.tf
+++ main.tf
@@ -24,3 +24,15 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = merge(local.common_tags, tomap({"lastUpdated" = timestamp()}))
 }
+
+data "azurerm_postgresql_server" "ethos_postgres_database" {
+  name                = "ethos-postgres-db-${var.env}"
+  resource_group_name = "ethos-postgres-db-${var.env}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = data.azurerm_postgresql_server.ethos_postgres_database.user_name
+  key_vault_id = module.key-vault.key_vault_id
+}
+
