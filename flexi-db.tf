a6144ed35af4750dd311ba1888f43f66cda1b75a
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-10-30 18:08:16 +0000

    DB Names

--- flexi-db.tf
+++ flexi-db.tf
@@ -5,7 +5,7 @@ module "postgres" {
   providers = {
     azurerm.postgres_network = azurerm.private_endpoint
   }
-  name          = "ecm-shared-postgres-v15"
+  name          = "ecm-consumer-postgres-v15"
   product       = var.product
   component     = var.component
   business_area = var.businessArea
@@ -13,35 +13,32 @@ module "postgres" {
   pgsql_databases = [
     {
       name : "ecmconsumer"
-    },
-    {
-      name : "ethos"
     }
   ]
   pgsql_version        = "15"
   admin_user_object_id = var.jenkins_AAD_objectId
 }
 
-resource "azurerm_key_vault_secret" "ecm_shared_postgres_user_v15" {
-  name         = "ecm-shared-postgres-user-v15"
+resource "azurerm_key_vault_secret" "ecm_consumer_postgres_user_v15" {
+  name         = "ecm-consumer-postgres-user-v15"
   value        = module.postgres.username
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "ecm_shared_postgres_password_v15" {
-  name         = "ecm-shared-postgres-password-v15"
+resource "azurerm_key_vault_secret" "ecm_consumer_postgres_password_v15" {
+  name         = "ecm-consumer-postgres-password-v15"
   value        = module.postgres.password
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "ecm_shared_postgres_host_v15" {
-  name         = "ecm-shared-postgres-host-v15"
+resource "azurerm_key_vault_secret" "ecm_consumer_postgres_host_v15" {
+  name         = "ecm-consumer-postgres-host-v15"
   value        = module.postgres.fqdn
   key_vault_id = module.key-vault.key_vault_id
 }
 
-resource "azurerm_key_vault_secret" "ecm_shared_postgres_port_v15" {
-  name         = "ecm-shared-postgres-port-v15"
+resource "azurerm_key_vault_secret" "ecm_consumer_postgres_port_v15" {
+  name         = "ecm-consumer-postgres-port-v15"
   value        = "5432"
   key_vault_id = module.key-vault.key_vault_id
 }
