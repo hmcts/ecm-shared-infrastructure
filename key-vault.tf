41ca59d0e45759fdde4d9dcc28e4048b476ca760
Dawid Strozak <dawid.strozak@hmcts.net>
2026-04-22 15:00:18 +0100

    Grant new jenkins mi per environment on the KV

--- key-vault.tf
+++ key-vault.tf
@@ -10,4 +10,10 @@ module "key-vault" {
   product_group_object_id = "414c132d-5160-42b3-bbff-43a2e1daafcf"
   common_tags             = local.common_tags
   create_managed_identity = true
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
